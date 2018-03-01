10eb8f625b02664fb294c62adf65baca9b23641d
AlexBO97 <alexandru_bondor@yahoo.com>
2018-03-01 20:02:18 +0000

    testing

--- bitmap_editor.rb
+++ bitmap_editor.rb
@@ -11,8 +11,10 @@ class BitmapEditor
 
     File.readlines(file).each do |line|
 
-      token = line.split(' ')
+      token = line.split(' ')           #             SPLITTING THE STRING INTO TOKENS
       command = token[0]
+
+
 
       case command
       when 'I'                          #             NEW IMAGE     
@@ -26,15 +28,18 @@ class BitmapEditor
           break 
         end
 
+
+
       when 'C'                          #             CLEAR IMAGE
         clear_image(image, lin, col)
+
+
 
       when 'L'                          #             COLOR ONE PIXEL
         lin_pixel = token[1].to_i
         col_pixel = token[2].to_i
         colour = token[3]
        
-        
         if correct_command(token, command)
           image[col_pixel][lin_pixel] = colour
         else
@@ -42,6 +47,8 @@ class BitmapEditor
           break
         end
         
+
+
       when 'V'                          # V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 
         x = token[1].to_i
         y1 = token[2].to_i
@@ -57,6 +64,8 @@ class BitmapEditor
           break
         end
     
+
+
       when 'H'                          # H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2  
         x1 = token[1].to_i
         x2 = token[2].to_i
@@ -65,13 +74,15 @@ class BitmapEditor
 
         if correct_command(token, command)
           for j in x1..x2
-            image[y][j] = colour
+           image[y][j] = colour 
           end 
         else
           puts "INVALID COMMAND FOR HORIZONTAL SEGMENT"
           break
         end
          
+
+        
       when 'S'
         print_image(image, lin, col)
       end
